11bdcb3b68b0473836a208c990b74f63203209cd
Pan Ziyue <random.rrr3@gmail.com>
2019-06-03 14:36:09 +0800

    set_terminate now performs environment checking

--- templates/set_terminate.sh
+++ templates/set_terminate.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# detect if system is running on ec2, exit if not on ec2
+http_code=$(curl --max-time 30 -s -o /dev/null -w "%{http_code}" "http://169.254.169.254/latest/meta-data/placement/availability-zone")
+if [[ $http_code == "404" ]]; then
+  # this is coded with the assumption that a non-AWS endpoint will return a 404
+  echo "This system is running on a cloud platform that is not AWS. Exiting..."
+  exit 0
+elif [[ $http_code == "000" ]]; then
+  # there might be a more robust way to do on-prem vs cloud detection than this
+  # this is coded with the assumption that the metadata IP is unreachable in on-prem environments
+  echo "Unable to get metadata, this system might be running on-premise. Exiting..."
+  exit 0
+fi
+
+if [[ $http_code != "200" ]]; then
+  echo "Unable to determine machine location, retrying with backoff..."
+  http_code=$(curl --max-time 10 --retry 5 -s -o /dev/null -w "%{http_code}" "http://169.254.169.254/latest/meta-data/placement/availability-zone")
+  if [[ $http_code != "200" ]]; then
+    echo "Exponential backoff failed, marking this service as failed"
+    exit 1
+  fi
+fi
+
+# system is confirmed to be in AWS due to HTTP 200
+
 # instance id
 az=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 region="`echo \"$az\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
